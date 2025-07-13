2e97ce910badf0ee60e65fa1dbe9258797c2f2b0
KiLO <gabrielbeckstrom@gmail.com>
2025-07-13 19:20:50 -0400

    Added Comments

--- Discord-Bot-Template/main.py
+++ Discord-Bot-Template/main.py
@@ -4,14 +4,21 @@ import logging
 from dotenv import load_dotenv
 import os
 
+# Get the Discord Token from the .env file.
+# NOTE: This is not tracked for security reasons. Create a .env file to hold the discord token.
+# - Load token securely from .env file.
+# - Retrieve the bot token from the environment
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+# Define bot intents: controls what events the bot recieves from Discord.
 intents = discord.Intents.default()
 intents.message_content = True
 
+### --- This is Example code to verify that the bot is working --- ###
+### MyClient class can be removed.
 class MyClient(discord.Client):
+    # Events: Bot is ready and connected
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -20,8 +27,12 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
+        # If the message content is "ping", reply with "pong"
         if message.content == 'ping':
             await message.channel.send('pong')
 
+# Creat an instance of the bot client, passing in the specified intents
 client = MyClient(intents=intents)
-client.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+# Run the bot
+client.run(token)
